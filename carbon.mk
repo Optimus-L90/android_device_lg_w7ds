dcf982522efd06526e0f307e7c6a1a590eb5492f
batlin1977 <batlin1977@gmail.com>
2015-09-07 21:51:33 +0200

    initial carbon bringup and some addition
    Change-Id: I462a98d1b802417de260fc233bd5e2f43d909681

--- carbon.mk
+++ carbon.mk
@@ -1,11 +1,5 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
-
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Configure dalvik heap
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
@@ -21,6 +15,6 @@ TARGET_SCREEN_HEIGHT := 960
 PRODUCT_RELEASE_NAME := LG L90
 PRODUCT_DEVICE := w7
 PRODUCK_MODEL := w7
-PRODUCT_NAME := slim_w7
+PRODUCT_NAME := carbon_w7
 PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_MANUFACTURER := LGE
